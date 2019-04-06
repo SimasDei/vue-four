655d958b40e013ea6d3449a8e08429ffee7ad8e2
SimasDei <simondeiman@gmail.com>
2019-04-06 21:56:01 +0300

    Alternate way to implement Components

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+
+import Home from './Components/Home.vue';
+
+Vue.component('Home', Home);
 
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});

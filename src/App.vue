4ddcdff50d923b0ad4d065ffeac3ef1fbbaaa844
SimasDei <simondeiman@gmail.com>
2019-04-06 23:54:28 +0300

    Use Dynamic components and switching

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-xs-12"></div>
+      <div class="col-xs-12">
+        <button @click="selectedComponent = 'appQuote'">Quote</button>
+        <button @click="selectedComponent = 'appAuthor'">Author</button>
+        <button @click="selectedComponent = 'appNew'">New</button>
+        <hr>
+        <p>{{selectedComponent}}</p>
+        <component :is="selectedComponent"></component>
+        <!-- <app-quote>
+          <h2 slot="title">Quote Heading</h2>
+          <p slot="content">Quote Text</p>
+        </app-quote>-->
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+import appQuote from "./Components/Quote.vue";
+import appAuthor from "./Components/Author";
+import appNew from "./Components/New";
+export default {
+  data: function() {
+    return {
+      selectedComponent: "appQuote"
+    };
+  },
+  components: {
+    appQuote,
+    appAuthor,
+    appNew
+  }
+};
 </script>
 
 <style>
---
+++ src/Components/Author.vue
@@ -0,0 +1,10 @@
+<template>
+  <h3>The Author</h3>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>

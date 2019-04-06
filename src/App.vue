b7785459075f2667c6b04dd5b8cbae1a091d7b3c
SimasDei <simondeiman@gmail.com>
2019-04-06 22:24:11 +0300

    project restructure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <Home/>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
 
-import Home from './Components/Home.vue';
-
-Vue.component('Home', Home);
-
 new Vue({
   el: '#app',
   render: h => h(App)
